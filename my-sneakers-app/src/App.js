92e40e48dabf824fe466af0ad6349e3e7ec36983
Gianpierre Fernandez <fgianpierre@gmail.com>
2020-03-12 01:39:26 +0100

    add bootstrap and node-sass, create components folder and sccs custon and import sneakers images

--- my-sneakers-app/src/App.js
+++ my-sneakers-app/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
+
+import ProductImage from './components/ProductImage'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+function App(props) {
+  console.log(props.inventory.bySize);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>Here will come my customize product</h1>
+        < ProductImage />
       </header>
     </div>
   );
---
+++ my-sneakers-app/src/components/ProductImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import redSneakers from '../images/red.jpg'
+
+function ProductImage() {
+
+    return(
+        <img src={redSneakers} alt=""/>
+    )
+}
+
+export default ProductImage;
